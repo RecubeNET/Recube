d8110a79df0e42b6fa6dd8971f92cda9f45c751a
turulix <tigga01@online.de>
2019-08-10 09:45:37 +0200

    Adding Enum to SpawnObjectPacket

--- Recube.Core/Network/Impl/Packets/Play/SpawnObjectPacket.cs
+++ Recube.Core/Network/Impl/Packets/Play/SpawnObjectPacket.cs
@@ -11,9 +11,41 @@ namespace Recube.Core.Network.Impl.Packets.Play
 	[Packet(0x00, NetworkPlayerState.Play)]
 	public class SpawnObjectPacket : IOutPacket
 	{
+		public enum SpawnType
+		{
+			Boat=1,
+			ItemStack=2,
+			AreaEffectCloud=3,
+			Minecart=10,
+			ActivatedTNT=50,
+			EnderCrystal=51,
+			TippedArrow=60,
+			Snowball=61,
+			Egg=62,
+			FireBall=63,
+			FireCharge=64,
+			ThrownEnderpearl=65,
+			WitherSkull=66,
+			ShulkerBullet=67,
+			LlamaSpit=68,
+			FallingObjects=70,
+			ItemFrame=71,
+			EyeOfEnder=72,
+			ThrownPotion=73,
+			ThrownExpBottle=75,
+			FireworkRocket=76,
+			LeashKnot=77,
+			ArmorStand=78,
+			EvocationFangs=79,
+			FishingHook=90,
+			SpectralArrow=91,
+			DragonFireball=93,
+			Trident=94
+		}
+		
 		public int EntityID;
 		public UUID UUID;
-		public byte Type;
+		public SpawnType Type;
 		public double X;
 		public double Y;
 		public double Z;
@@ -28,7 +60,7 @@ namespace Recube.Core.Network.Impl.Packets.Play
 		{
 			buffer.WriteVarInt(EntityID);
 			buffer.WriteStringWithLength(UUID.ToString());
-			buffer.WriteByte(Type);
+			buffer.WriteByte((byte) Type);
 			buffer.WriteDouble(X);
 			buffer.WriteDouble(Y);
 			buffer.WriteDouble(Z);
