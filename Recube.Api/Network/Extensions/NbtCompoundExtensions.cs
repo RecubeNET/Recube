dd5ea53bdc3322f8f3834437df6ef9bb32d6144d
turulix <tigga01@online.de>
2019-08-18 15:43:43 +0200

    Adding World Settings

--- Recube.Api/Network/Extensions/NbtCompoundExtensions.cs
+++ Recube.Api/Network/Extensions/NbtCompoundExtensions.cs
@@ -11,10 +11,20 @@ namespace Recube.Api.Network.Extensions
 			return nbt;
 		}
 
+		public static NbtString? GetString(this NbtCompound nbt, string tag)
+		{
+			return nbt.Get<NbtString>(tag);
+		}
+
 		public static NbtCompound AddByte(this NbtCompound nbt, string tag, byte value)
 		{
 			nbt[tag] = new NbtByte(tag, value);
 			return nbt;
+		}
+
+		public static NbtByte? GetByte(this NbtCompound nbt, string tag)
+		{
+			return nbt.Get<NbtByte>(tag);
 		}
 
 		public static NbtCompound AddDouble(this NbtCompound nbt, string tag, double value)
@@ -23,10 +33,20 @@ namespace Recube.Api.Network.Extensions
 			return nbt;
 		}
 
+		public static NbtDouble? GetDouble(this NbtCompound nbt, string tag)
+		{
+			return nbt.Get<NbtDouble>(tag);
+		}
+
 		public static NbtCompound AddLong(this NbtCompound nbt, string tag, long value)
 		{
 			nbt[tag] = new NbtLong(tag, value);
 			return nbt;
+		}
+
+		public static NbtLong? GetLong(this NbtCompound nbt, string tag)
+		{
+			return nbt.Get<NbtLong>(tag);
 		}
 
 		public static NbtCompound AddInt(this NbtCompound nbt, string tag, int value)
@@ -35,10 +55,20 @@ namespace Recube.Api.Network.Extensions
 			return nbt;
 		}
 
+		public static NbtInt? GetInt(this NbtCompound nbt, string tag)
+		{
+			return nbt.Get<NbtInt>(tag);
+		}
+
 		public static NbtCompound AddNbtCompound(this NbtCompound nbt, NbtCompound value)
 		{
-			nbt[value.Name ?? throw new NullReferenceException("NbtCompound can't be null")] = value;
+			nbt[value.Name ?? throw new NullReferenceException("NbtCompound name can't be null")] = value;
 			return nbt;
+		}
+
+		public static NbtCompound? GetNbtCompound(this NbtCompound nbt, string tag)
+		{
+			return nbt.Get<NbtCompound>(tag);
 		}
 	}
 }
